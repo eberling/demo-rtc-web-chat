273cc34cceaebfcff95bb876a20b5864e11b3f0b
eberling <c.eberling93@gmail.com>
2020-09-14 22:34:55 +0200

    init, first runnable webcam

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CreateRoom from "./routes/createRoom";
+import Room from "./routes/Room";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={CreateRoom} />
+          <Route path="/room/:roomID" component={Room} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
